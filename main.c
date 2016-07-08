c7c8baad5b39a2649675c6cdddfbbad3bfd62303
Mike Henry <mhenry@handycodejob.com>
2016-07-08 01:03:38 -0600

    working on game logic

--- main.c
+++ main.c
@@ -1,71 +1,28 @@
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-#define MAXRCVLEN 500
-#define PORTNUM 2300
 
 int main() {
-  char buffer[MAXRCVLEN]; /* +1 so we can add null terminator */
-  int len, mysocket;
-  struct sockaddr_in dest;
+  char p1throw[2];
+  char p2throw[] = "sc";
 
-  mysocket = socket(AF_INET, SOCK_STREAM, 0);
+  printf("What would you like to throw? \n");
 
-  memset(&dest, 0, sizeof(dest)); /* zero the struct */
-  dest.sin_family = AF_INET;
-  dest.sin_addr.s_addr = htonl(
-      INADDR_LOOPBACK); /* set destination IP number - localhost, 127.0.0.1*/
-  dest.sin_port = htons(PORTNUM); /* set destination port number */
+  printf("Rock (r)\nPaper (p)\nScissors (sc)\nLizard (l)\nSpock(sp)\n");
 
-  connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr));
-
-  len = recv(mysocket, buffer, MAXRCVLEN, 0);
-
-  /* We have to null terminate the received data
-   * ourselves */
-
-  printf("Received %s (%d bytes).\n", buffer, len);
-
-  close(mysocket);
-  //return EXIT_SUCCESS;
-
-  char *msg = "Hello World !\n\0";
-
-  struct sockaddr_in dest_dest; /* socket info about the machine connecting to us */
-  struct sockaddr_in serv; /* socket info about our server */
-  int mysocket_dest;            /* socket used to listen for incoming connections */
-  socklen_t socksize = sizeof(struct sockaddr_in);
-
-  memset(&serv, 0,
-         sizeof(serv));      /* zero the struct before filling the fields */
-  serv.sin_family = AF_INET; /* set the type of connection to TCP/IP */
-  serv.sin_addr.s_addr =
-      htonl(INADDR_ANY);          /* set our address to any interface */
-  serv.sin_port = htons(PORTNUM); /* set the server port number */
-
-  mysocket_dest = socket(AF_INET, SOCK_STREAM, 0);
-
-  /* bind serv information to mysocket */
-  bind(mysocket_dest, (struct sockaddr *)&serv, sizeof(struct sockaddr));
-
-  /* start listening, allowing a queue of up to 1 pending connection */
-  listen(mysocket_dest, 1);
-  int consocket = accept(mysocket_dest, (struct sockaddr *)&dest_dest, &socksize);
-
-  while (consocket) {
-    printf("Incoming connection from %s - sending welcome\n",
-           inet_ntoa(dest_dest.sin_addr));
-    send(consocket, msg, strlen(msg), 0);
-    close(consocket);
-    consocket = accept(mysocket_dest, (struct sockaddr *)&dest_dest, &socksize);
+  if (fgets(p1throw, sizeof(p1throw), stdin) == NULL) {
+    printf("couldn't read your input");
   }
+  p1throw [strcspn(p1throw, "\n")] = 0;
 
-  close(mysocket_dest);
+  printf("You threw %s \n", p1throw);
+
+  printf("The computer thew %s \n", p2throw);
+
+  if (strcmp(p1throw, "r") == 0) {
+    printf("good job\n");
+  } else {
+    printf("you lost\n");
+  }
   return EXIT_SUCCESS;
 }
---
+++ network_main.c
@@ -0,0 +1,72 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define MAXRCVLEN 500
+#define PORTNUM 2300
+
+int main() {
+  char buffer[MAXRCVLEN]; /* +1 so we can add null terminator */
+  int mysocket;
+  ssize_t len;
+  struct sockaddr_in dest;
+
+  mysocket = socket(AF_INET, SOCK_STREAM, 0);
+
+  memset(&dest, 0, sizeof(dest)); /* zero the struct */
+  dest.sin_family = AF_INET;
+  dest.sin_addr.s_addr = htonl(
+      INADDR_LOOPBACK); /* set destination IP number - localhost, 127.0.0.1*/
+  dest.sin_port = htons(PORTNUM); /* set destination port number */
+
+  connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr));
+
+  len = recv(mysocket, buffer, MAXRCVLEN, 0);
+
+  /* We have to null terminate the received data
+   * ourselves */
+
+  printf("Received %s (%zd bytes).\n", buffer, len);
+
+  close(mysocket);
+  //return EXIT_SUCCESS;
+
+  char *msg = "Hello World !\n\0";
+
+  struct sockaddr_in dest_dest; /* socket info about the machine connecting to us */
+  struct sockaddr_in serv; /* socket info about our server */
+  int mysocket_dest;            /* socket used to listen for incoming connections */
+  socklen_t socksize = sizeof(struct sockaddr_in);
+
+  memset(&serv, 0,
+         sizeof(serv));      /* zero the struct before filling the fields */
+  serv.sin_family = AF_INET; /* set the type of connection to TCP/IP */
+  serv.sin_addr.s_addr =
+      htonl(INADDR_ANY);          /* set our address to any interface */
+  serv.sin_port = htons(PORTNUM); /* set the server port number */
+
+  mysocket_dest = socket(AF_INET, SOCK_STREAM, 0);
+
+  /* bind serv information to mysocket */
+  bind(mysocket_dest, (struct sockaddr *)&serv, sizeof(struct sockaddr));
+
+  /* start listening, allowing a queue of up to 1 pending connection */
+  listen(mysocket_dest, 1);
+  int consocket = accept(mysocket_dest, (struct sockaddr *)&dest_dest, &socksize);
+
+  while (consocket) {
+    printf("Incoming connection from %s - sending welcome\n",
+           inet_ntoa(dest_dest.sin_addr));
+    send(consocket, msg, strlen(msg), 0);
+    close(consocket);
+    consocket = accept(mysocket_dest, (struct sockaddr *)&dest_dest, &socksize);
+  }
+
+  close(mysocket_dest);
+  return EXIT_SUCCESS;
+}
