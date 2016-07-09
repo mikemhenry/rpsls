18540ad79ea8c16ef1a788e9f86e9612dac4aab4
Mike Henry <mhenry@handycodejob.com>
2016-07-08 18:47:38 -0600

    fixed input and mapping

--- main.c
+++ main.c
@@ -2,18 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
+void getThrowCode(char throw [3]) {
+
+  int i_throw = (int)throw [0];
+  i_throw += (int)throw [1];
+
+  switch (i_throw) {
+  case 225:
+    i_throw = 0;
+    break;
+  case 209:
+    i_throw = 1;
+    break;
+  case 214:
+    i_throw = 2;
+    break;
+  case 213:
+    i_throw = 3;
+    break;
+  case 227:
+    i_throw = 4;
+    break;
+  }
+
+  printf("ASCII code throw %d\n", i_throw);
+}
+
+void getThrow(char throw[3]) {
+  if (fgets(throw, 3, stdin) == NULL) {
+    printf("Couldn't read your input");
+  }
+  //throw[strcspn(throw, "\n")] = 0;
+}
+
 int main() {
-  char p1throw[2];
-  char p2throw[] = "sc";
+  char p1throw[3];
+  char p2throw[] = "2";
 
   printf("What would you like to throw? \n");
 
-  printf("Rock (r)\nPaper (p)\nScissors (sc)\nLizard (l)\nSpock(sp)\n");
-
-  if (fgets(p1throw, sizeof(p1throw), stdin) == NULL) {
-    printf("couldn't read your input");
-  }
-  p1throw [strcspn(p1throw, "\n")] = 0;
+  printf("Rock (ro)\nPaper (pa)\nScissors (sc)\nLizard (li)\nSpock(sp)\n");
+  getThrow(p1throw);
 
   printf("You threw %s \n", p1throw);
 
@@ -24,5 +53,7 @@ int main() {
   } else {
     printf("you lost\n");
   }
+  getThrowCode(p1throw);
+
   return EXIT_SUCCESS;
 }
